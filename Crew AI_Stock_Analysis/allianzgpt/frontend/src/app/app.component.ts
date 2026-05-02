2df532113f4b46f760f8b1042b029a60b9f08707
drdeveloper88 <dr.developer88@gmail.com>
2026-05-02 11:23:19 +0530

    Delete Crew AI_Stock_Analysis directory

--- Crew AI_Stock_Analysis/allianzgpt/frontend/src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: '<app-chat></app-chat>',
-  styles: ['body { font-family: Arial, sans-serif; }']
-})
-export class AppComponent {}
